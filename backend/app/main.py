dcce6355e79d4d66562cbffd2c633264a89b08aa
LazyCoderror101 <hazardemperor9@gmail.com>
2026-07-04 19:18:18 +0530

    Add PDF upload functionality

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+import shutil
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI(
     title="TechDocAI",
-    descryption= "Multimodal RAG System for Technical Documents",
+    description="Multimodal RAG System for Technical Documents",
     version="1.0.0"
 )
 
@@ -13,4 +14,23 @@ def home():
         "project":"TechDocAI",
         "message": "Backend running Successfully......",
         "version": "1.0.0"
+    }
+
+@app.post("/upload")
+async def upload_pdf(
+    file: UploadFile = File(...)
+):
+    file_path = f"storage/{file.filename}"
+    
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
+
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "path": file_path,
+        "message": "PDF Saved successfully"
     }
